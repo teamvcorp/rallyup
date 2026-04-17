059d6a616082c201889157dba1e357702702ee91
Robert Von Der Becke <teamvcorp@thevacorp.com>
2026-04-17 14:27:09 -0500

    headlessui dependancy

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,6 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-04-30.basil',
+  apiVersion: '2026-03-25.dahlia',
   typescript: true,
 })
